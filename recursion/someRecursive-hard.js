29a51a510399608f54d60a3c5b6459df47c0f3c8
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-09-28 11:44:30 -0400

    accidentally erased

--- recursion/someRecursive-hard.js
+++ recursion/someRecursive-hard.js
@@ -1,24 +1,11 @@
 // SAMPLE INPUT / OUTPUT
-const isOdd = val => val % 2 !== 0;
-
-/**
- * The function returns true if a single value in the array returns true when
- * passed to the callback. Otherwise it returns false.
- * @param {Array} arr
- * @param {callback} cb
- * @returns boolean
- */
-const someRecursive = (arr, cb) => {
-  // what is the condition that ends
-  if (cb(arr[0])) {
-    return true;
-  }
-  return false;
-};
-
-console.log(someRecursive([1, 2, 3, 4], isOdd));
+const isOdd = (val) => val % 2 !== 0;
 
 // someRecursive([1,2,3,4], isOdd) // true
 // someRecursive([4,6,8,9], isOdd) // true
 // someRecursive([4,6,8], isOdd) // false
 // someRecursive([4,6,8], val => val > 10); // false
+
+const someRecursive = (arr, cb) => {};
+
+console.log(someRecursive([1, 2, 3, 4], isOdd));
