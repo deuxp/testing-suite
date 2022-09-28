90984153fe55e52fa7317b8faa568d87b210cd6b
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-09-28 14:05:30 -0400

    so close to a solo answer

--- recursion/flatten.js
+++ recursion/flatten.js
@@ -4,17 +4,24 @@
  * @returns an array of items that are no longer nested in sub-arrays
  */
 const flatten = arr => {
-  const newArr = [];
-  // what is the condition that says this operation should finish
-  if (!Array.isArray(arr[0])) {
-    return arr[0];
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    // if item is a num then push to new array and return recursive call
+    if (!Array.isArray(arr[i])) {
+      newArr.push(arr[i]);
+    } else {
+      // new array needs the old array with the concatenation of the function digging out the next thing
+      newArr = newArr.concat(flatten(arr[i]));
+    }
   }
-  // if it is an array // dig it out (the first index)
-  // recall - this will be called until first item is not an array
-  return newArr.concat(flatten(arr[0]));
+  return newArr;
 };
 
-console.logflatten([1, 2, 3, [4, 5]]); // [1, 2, 3, 4, 5]
+// console.log(flatten([1])); // [1]
+// console.log(flatten([1, 2, 3, [4, 5]])); // [1, 2, 3, 4, 5]
 // console.log(flatten([1, [2, [3, 4], [[5]]]])); // [1, 2, 3, 4, 5]
 // console.log(flatten([[1], [2], [3]])); // [1,2,3]
-// console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3
+
+// This one was very hard. I came really close to a solution, but in the end
+// I had to peek at the answer for the loop logic. So close.
