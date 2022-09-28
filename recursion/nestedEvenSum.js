c45e803cb5d847e70676a11ccc561b0477c917f5
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-09-28 19:46:43 -0400

    set ups

---
+++ recursion/nestedEvenSum.js
@@ -0,0 +1,5 @@
+/**
+ * Returns the sum of all even numbers in an object which may contain nested objects.
+ * @param {{{}}} obj nested objects, containing numbers
+ */
+const nestedEvenSum = obj => {};
