8846191bd2043aabe9f681af804b055160a15722
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-09-28 11:55:59 -0400

    passed the mustard

--- recursion/someRecursive-hard.js
+++ recursion/someRecursive-hard.js
@@ -10,15 +10,16 @@ const isOdd = val => val % 2 !== 0;
  */
 const someRecursive = (arr, cb) => {
   // what is the condition that ends
+  if (arr.length === 0) return false;
+
   if (cb(arr[0])) {
     return true;
   }
-  return false;
+  return someRecursive(arr.slice(1), cb);
 };
 
-console.log(someRecursive([1, 2, 3, 4], isOdd));
+console.log(someRecursive([1, 2, 3, 4], isOdd)); //true
 
-// someRecursive([1,2,3,4], isOdd) // true
-// someRecursive([4,6,8,9], isOdd) // true
-// someRecursive([4,6,8], isOdd) // false
+console.log(someRecursive([4, 6, 8, 9], isOdd)); // true
+console.log(someRecursive([4, 6, 8], isOdd)); // false
 // someRecursive([4,6,8], val => val > 10); // false
