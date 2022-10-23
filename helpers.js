3022f8762c603dbd4774a64db9b24dc5b4930817
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-10-22 21:27:31 -0400

    range object

--- helpers.js
+++ helpers.js
@@ -20,7 +20,8 @@ module.exports = {
     return [left, right];
   },
 
-  range = (start, stop, step) => Array.from({length: stop - start / step +1}, (_, i) => i + (start * step))
+  range: (start, stop, step) =>
+    Array.from({ length: stop - start / step + 1 }, (_, i) => i + start * step),
 
   /** Merges two sorted arrays of any length - - - - - - - - - - - - - - - - - - - -
    * @param {arr1:Array} Sorted Array to be merged with arr2
